ce281d465a50171959455eecc7273b986d4ee391
YouLinNG <903607767@qq.com>
2020-06-13 01:23:37 +0800

    analysis

---
+++ src/main/java/nju/passport/model/MonthVisa.java
@@ -0,0 +1,31 @@
+package nju.passport.model;
+
+/**
+ * Created with IDEA
+ * author:yanghaolin
+ * Date:2020/6/13
+ * Time:00:23
+ * Description：
+ */
+public class MonthVisa {
+
+    private String month;
+
+    private int num;
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
---
+++ src/main/java/nju/passport/model/Year.java
@@ -0,0 +1,31 @@
+package nju.passport.model;
+
+/**
+ * Created with IDEA
+ * author:yanghaolin
+ * Date:2020/6/12
+ * Time:22:47
+ * Description：
+ */
+public class Year {
+
+    private String item;
+
+    private int count;
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
